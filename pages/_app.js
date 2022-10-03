fe92704331a0613f7f6c652bb2617c57b230c5f8
Yolit Zacarias <yolit.zr@gmail.com>
2022-10-03 16:26:29 -0300

    Prueba

--- pages/_app.js
+++ pages/_app.js
@@ -19,8 +19,7 @@ function MyApp({ Component, pageProps }) {
 		const handleRouteChange = (url) => {
 			ga.pageview(url);
 		};
-		//When the component is mounted, subscribe to router changes
-		//and log those page views
+
 		router.events.on('routeChangeComplete', handleRouteChange);
 
 		// If the component is unmounted, unsubscribe
